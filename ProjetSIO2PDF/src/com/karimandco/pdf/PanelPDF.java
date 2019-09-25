2f16dad34b1759dee113e2a15500b9bee5b276af
TomLeal <leal.tom.lurcat@gmail.com>
2019-09-25 15:36:59 +0200

    Derniere version apres présentation 2.

--- ProjetSIO2PDF/src/com/karimandco/pdf/PanelPDF.java
+++ ProjetSIO2PDF/src/com/karimandco/pdf/PanelPDF.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.karimandco.pdf;
 
 import com.itextpdf.text.BadElementException;
@@ -22,7 +17,8 @@ import javax.swing.JOptionPane;
 public class PanelPDF extends javax.swing.JPanel {
 
     /**
-     * Creates new form PanelPDF
+     * Crée le composant PanelPDF.
+     * 
      */
     public PanelPDF() {
         initComponents();
